08c6ed49e660c2ede110a13e53d02be4926c2327
srg206 <you@example.com>
2025-02-05 14:03:23 +0300

    upd

--- graphql/backend/resolvers/resolvers.py
+++ graphql/backend/resolvers/resolvers.py
@@ -7,15 +7,13 @@ query = QueryType()
 user_type = ObjectType("User")
 event_type = ObjectType("Event")
 
-
 @query.field("users")
 def resolve_users(_, info):
     return users_data
 
 @query.field("events")
-def resolve_events(_, info, importance: int = None, user_id: int = None, name: str = None,date: str = None): # Добавляем аргументы
+def resolve_events(_, info, importance: int = None, user_id: int = None, date: str = None, name: str = None): # Добавляем аргументы
     filtered_events = events_data  # Начинаем с полного списка
-
     if importance is not None:
         filtered_events = [event for event in filtered_events if event["importance"] == importance]
     if user_id is not None:
@@ -25,7 +23,6 @@ def resolve_events(_, info, importance: int = None, user_id: int = None, name: s
     if date is not None:
         target_date= date
         filtered_events = [event for event in filtered_events if event["date"] == target_date]
-
     return filtered_events
 
 @query.field("user")
@@ -37,6 +34,25 @@ def resolve_user(_, info, id):
 def resolve_event(_, info, id):
     event = next((event for event in events_data if event["id"] == id), None)
     return event
+
+@user_type.field("events")
+def resolve_user_events(user, info, event_id: int = None, name: str = None, date: str = None, importance: int =None):
+    events = [event for event in events_data if event["user_id"] == user["id"]]
+    
+    if event_id is not None:
+        events = [event for event in events if event["id"] == event_id]
+    if name is not None:
+        events = [event for event in events if event["name"] == name]
+    if date is not None:
+        events = [event for event in events if event["date"] == date]
+    if importance is not None:
+        events = [event for event in events if event["importance"] == importance]
+    return events
+@event_type.field("user")
+def resolve_event_user(event, info):
+    user = next((user for user in users_data if user["id"] == event["user_id"]), None)
+    return user
+
 
 
 schema = make_executable_schema(type_defs, query, user_type, event_type)
